ee5426bba2533dd2a293f8b52829333a0af8573b
MansiPathak <pathakmansi608@gmail.com>
2026-03-31 11:23:04 +0530

    ok

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,8 +1,12 @@
+// routes/reviews.js
 const express = require('express');
 const router  = express.Router();
-const { getReviews, addReview } = require('../controllers/reviewController');
+const { getReviews, addReview, getHospitalReviews } = require('../controllers/reviewController');
 
-router.get('/',  getReviews);
-router.post('/', addReview);
+router.get('/',                      getReviews);
+router.get('/hospital/:hospitalId',  getHospitalReviews);   // ← new
+router.post('/',                     addReview);
 
 module.exports = router;
+
+
